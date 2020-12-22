4ac4fd05a9ec48f37bca5a8cfcef272292e10e4f
nathanwagstaff <natewagstaff21@gmail.com>
2020-12-22 13:26:30 -0700

    Next update

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
+        <h1>Birthday</h1>
+        <BirthdayForm />
       </div>
     );
   }
---
+++ src/components/birthdayForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BirthdayForm extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    render () {
+        return <h1>BirthdayForm Component</h1>
+    }
+}
+
+export default BirthdayForm;
