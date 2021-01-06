fbf753a7dbdf2ab2b3b23003e537dc640d0effb6
nathanwagstaff <natewagstaff21@gmail.com>
2021-01-05 17:39:27 -0700

    set up state and handleChange and moment.js

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
+import moment from 'moment';
 
 class BirthdayForm extends Component {
     
     constructor(props) {
         super(props);
+
+        this.state = {
+            startDate: moment(),
+
+        }
+
+        this.handleChange = this.handleChange.bind(this)
     }
+    
+    handleChange(date){
+        this.setState({
+            startDate: date
+        })
+    }   
+
     render () {
         return(
             <div>
                 <h1>BirthdayForm Component</h1>
-                <DatePicker />
+                <DatePicker 
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
             </div>
         )
     }
