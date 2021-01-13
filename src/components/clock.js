9dd974f3b401aec0980d5600222b4d9b01a30498
nathanwagstaff <natewagstaff21@gmail.com>
2021-01-12 18:41:24 -0700

    added age feature

--- src/components/clock.js
+++ src/components/clock.js
@@ -33,15 +33,31 @@ export default class Clock extends Component {
             'seconds': seconds
         }
     }
+
+    getAge = function() {
+        var bday = new Date(this.birthday)
+        let today = new Date();
+
+        var distance = today.getTime() - bday.getTime();
+        var daysOld = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var yearsOld = Number((daysOld/365).toFixed(0))
+        return yearsOld
+    }.bind(this)
+
     render(){
         const data = this.state.timeRemaining 
 
         return(
             <div>
-                <div>DAYS  {data.days} </div>
-                <div>HRS {data.hours} </div>
-                <div>MINS  {data.minutes} </div>
-                <div>SECS  {data.seconds} </div>
+                <div>
+                    <div>DAYS  {data.days} </div>
+                    <div>HRS {data.hours} </div>
+                    <div>MINS  {data.minutes} </div>
+                    <div>SECS  {data.seconds} </div>
+                </div>
+                <div>
+                    {<h4>remaining until you are {this.getAge()}</h4>}
+                </div>
             </div>
         )
 
