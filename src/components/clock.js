16bfdf98c021c2b40b7f5a18050ea3a96364a67d
nathanwagstaff <natewagstaff21@gmail.com>
2021-01-14 18:39:29 -0700

    fix for same day

--- src/components/clock.js
+++ src/components/clock.js
@@ -41,6 +41,8 @@ export default class Clock extends Component {
     
             else if (birthDay < currentDay) {
                 bday.setFullYear(today.getFullYear() + 1);
+            } else if (birthDay == currentDay) {
+                return 0
             }
         }
 
@@ -75,15 +77,23 @@ export default class Clock extends Component {
 
         return(
             <div>
-                <div>
-                    <div>DAYS  {data.days} </div>
-                    <div>HRS {data.hours} </div>
-                    <div>MINS  {data.minutes} </div>
-                    <div>SECS  {data.seconds} </div>
-                </div>
-                <div>
-                    {<h4>remaining until you are {this.getAge()}</h4>}
-                </div>
+                {
+                    this.state.timeRemaining == 0 ?
+                        <h1>Happy Birthday! </h1>
+                        :
+                    <div>
+                        <div>
+                            <div>DAYS  {data.days} </div>
+                            <div>HRS {data.hours} </div>
+                            <div>MINS  {data.minutes} </div>
+                            <div>SECS  {data.seconds} </div>
+                        </div>
+                        <div>
+                            {<h4>remaining until you are {this.getAge()}</h4>}
+                        </div>
+                    </div>
+                }
+                
             </div>
         )
 
